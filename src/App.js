29dc9b76cdc74e7859c86c67d59ece6af89b2ab0
JamePut <110074605+JamePut@users.noreply.github.com>
2023-08-25 13:46:18 +0700

    create refs-func.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,20 @@ import {Calendar} from './class-components';*/
 import './App.css';
 /*import logo from './logo.svg';*/
 import './style.css';
-import Banner from './banner'
+/*import Banner from './banner'
 import {Button} from './class-components';
 import {Calculator} from './calculator';
 import {Calculator2} from './calculator';
 import {EventData2} from './event-data';
-import {Table} from './event-data';
+import {Table} from './event-data';*/
+import RefsFunc from './refs-func';
 
 
 function App() {
   return(
         <>
-        <Banner/>
         <center>
-          <Button/>
-          <br/>
-          <Calculator/>
-          <br/>
-          <Calculator2/>
-          <br/>
-          <EventData2/>
-          <Table/>
+          <RefsFunc/>
           <br/>
         </center>
         </>
